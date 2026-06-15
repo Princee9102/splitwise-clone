3e984f73554bf1e28578f96a8601329a543e6825
Prince Vishwakarma <152499041+Princee9102@users.noreply.github.com>
2026-06-16 00:36:25 +0530

    Fix production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://splitwise-clone-46n9.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
